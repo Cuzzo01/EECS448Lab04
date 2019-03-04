a1bb91bf89dbf38d040834661ea92d90d9dc5f98
Cuzzo01 <2738751+Cuzzo01@users.noreply.github.com>
2019-03-04 13:27:08 -0600

    Actually finish Exercise#3
    Forgot to actually write the email validation. Exercise should be actually complete now.

--- Exercise3/formChecker.js
+++ Exercise3/formChecker.js
@@ -6,8 +6,6 @@ function validateForm() {
   let mexicanCokeNum = document.forms['overallForm']['mexicanCokeNum'].value;
   let shipping = document.forms['overallForm']['shipping'].value;
 
-  console.log("checking form")
-
   if (shipping == ""
     || pass == ""
     || cokeNum == ""
@@ -20,7 +18,37 @@ function validateForm() {
       return false;
     }
 
-  // Validate email is an email address
-  console.log("checks passed")
+  // States: (what is bring read through)
+  // 0: name before @
+  // 1: domain directly after @ (cannot be a TLD)
+  // 2: any domain after first (coule be TLD or subdomain)
+  let state = 0;
+  let buffer = "";
+  for (var i = 0; i < user.length; i++) {
+    let currentChar = user.charAt(i);
+    if (currentChar == '@' && state = 0) {
+      if (buffer.length > 0) {
+        state = 1;
+        buffer = "";
+      } else {
+        // no name before @ sign
+        return false;
+      }
+    } else if (currentChar == '.' && state == 1) {
+      if (buffer.length < 1) {
+        // no domain before period
+        return false;
+      } else {
+        state = 2;
+      }
+      buffer = "";
+    } else {
+      buffer += currentChar;
+    }
+  }
+  if (buffer.length < 1 || state != 2) {
+    // no domain after final '.' or didnt make through all states
+    return false;
+  }
   return true;
 }
